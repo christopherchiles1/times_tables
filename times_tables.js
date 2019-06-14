658a990fcc3809dd29864b8c01b84423442a77f9
Chris Chiles <christopher.chiles1@gmail.com>
2019-06-14 14:24:05 -0700

    clean up code into smaller functions

--- times_tables.js
+++ times_tables.js
@@ -8,7 +8,7 @@ function setup() {
   circleLocation = createVector(radius + margin, radius + margin);
   menuLocation   = createVector(2 * radius + 3 * margin, margin)
 
-  modulusSlider = createSlider(10, 500, 200);
+  modulusSlider = createSlider(10, 300, 200);
   modulusSlider.position(menuLocation.x, menuLocation.y);
 
   multiplierSlider = createSlider(0, 100, 12);
@@ -16,24 +16,45 @@ function setup() {
 }
 
 function draw() {
-  let modulus = modulusSlider.value();
   let multiplier = multiplierSlider.value();
-  
-  let vectors = [];
-  for(let i = 0; i < modulus; i++) {
-    let angle = i * TWO_PI / modulus;
-    newVector = unitVector.copy().rotate(angle).add(circleLocation);
-    vectors.push(newVector);
-  }
+  let modulus    = modulusSlider.value();
+  let points     = getCirclePoints(multiplier, modulus);
 
   background(255);
 
-  vectors.forEach(function(vector) {
-    circle(vector.x, vector.y, 1);
-  });
+  drawCirclePoints(points);
+  drawConnections(points, multiplier, modulus);
+  drawLabels();
+}
 
-  vectors.forEach(function(vector, index) {
-    let toVector = vectors[(index * multiplier) % modulus];
-    line(vector.x, vector.y, toVector.x, toVector.y);
+function getCirclePoints(multiplier, modulus) {
+  let points = [];
+  for(let i = 0; i < modulus; i++) {
+    let angle = i * TWO_PI / modulus;
+    nextPoint = unitVector.copy().rotate(angle).add(circleLocation);
+    points.push(nextPoint);
+  }
+
+  return points;
+}
+
+function drawCirclePoints(points) {
+  points.forEach(function(point) {
+    circle(point.x, point.y, 1);
   });
+}
+
+function drawConnections(points, multiplier, modulus) {
+  points.forEach(function(point, index) {
+    let toPoint = points[(index * multiplier) % modulus];
+    line(point.x, point.y, toPoint.x, toPoint.y);
+  });
+}
+
+function drawLabels() {
+  fill(0);
+  textSize(16);
+  textAlign(LEFT);
+  text(`Multiplier (${multiplierSlider.value()})`, multiplierSlider.x + multiplierSlider.width + 15, multiplierSlider.y + 8);
+  text(`Modulus (${modulusSlider.value()})`, modulusSlider.x + modulusSlider.width + 15, modulusSlider.y + 8);
 }
