c891131fdc34ff188e1e25e16cd26bc0b75aeb8e
Chris Chiles <christopher.chiles1@gmail.com>
2019-06-14 11:58:16 -0700

    add sliders for modulus and multiplier

--- times_tables.js
+++ times_tables.js
@@ -1,26 +1,31 @@
 function setup() {
   createCanvas(720, 405);
 
-  radius       = (height / 2) - 15;
-  unitVector   = createVector(radius, 0);
-  offsetVector = createVector(radius + 15, radius + 15);
+  let margin = 15;
 
-  // Variables (to be controlled interactively)
-  modulus = 200;
-  multiplier = 12;
+  radius         = (height / 2) - margin;
+  unitVector     = createVector(radius, 0);
+  circleLocation = createVector(radius + margin, radius + margin);
+  menuLocation   = createVector(2 * radius + 3 * margin, margin)
 
-  // find points along circle
-  vectors = [];
-  for(let i = 0; i < modulus; i++) {
-    let angle = i * TWO_PI / modulus;
-    newVector = unitVector.copy().rotate(angle).add(offsetVector);
-    vectors.push(newVector);
-  }
+  modulusSlider = createSlider(10, 500, 200);
+  modulusSlider.position(menuLocation.x, menuLocation.y);
 
-  noLoop();
+  multiplierSlider = createSlider(0, 100, 12);
+  multiplierSlider.position(menuLocation.x, menuLocation.y + 25);
 }
 
 function draw() {
+  let modulus = modulusSlider.value();
+  let multiplier = multiplierSlider.value();
+  
+  let vectors = [];
+  for(let i = 0; i < modulus; i++) {
+    let angle = i * TWO_PI / modulus;
+    newVector = unitVector.copy().rotate(angle).add(circleLocation);
+    vectors.push(newVector);
+  }
+
   background(255);
 
   vectors.forEach(function(vector) {
